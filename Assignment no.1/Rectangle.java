a4401ffee9ded2c470d22b8cfd4470157e519a93
Shivam Raut <shivamraut2002@gmail.com>
2026-08-22 18:10:21 +0530

    Delete Assignment no.1 directory

--- Assignment no.1/Rectangle.java
+++
@@ -1,8 +0,0 @@
-public class Rectangle{
-public static void main(String[] args){
-  int length = 6;
-   int width = 4;
-    int area = length * width;
-  System.out.println("Area of Rectangle = " + area);
-}
-}
--- Assignment no.1/Square.java
+++
@@ -1,7 +0,0 @@
-public class Square{
-    public static void main(String[] args){
-        int side = 8;
-        int area = side * side;
-  System.out.println("Area of Square = " + area);
- }
-}
