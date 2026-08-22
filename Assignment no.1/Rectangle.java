3941e860faa42e1a24128e1984680f4804920311
Shivam Raut <shivamraut2002@gmail.com>
2026-08-22 18:04:27 +0530

    Add files via upload

---
+++ Assignment no.1/Rectangle.java
@@ -0,0 +1,8 @@
+public class Rectangle{
+public static void main(String[] args){
+  int length = 6;
+   int width = 4;
+    int area = length * width;
+  System.out.println("Area of Rectangle = " + area);
+}
+}
---
+++ Assignment no.1/Square.java
@@ -0,0 +1,7 @@
+public class Square{
+    public static void main(String[] args){
+        int side = 8;
+        int area = side * side;
+  System.out.println("Area of Square = " + area);
+ }
+}
