4d0a3ba09aa3025ed19b08698155ed740352cc97
Shivam Raut <shivamraut2002@gmail.com>
2026-08-22 18:05:17 +0530

    Add files via upload

---
+++ Assignment no. 2/GradeEvaluation.java
@@ -0,0 +1,20 @@
+class GradeEvaluation{
+    public static void main(String[ ] args) {
+         int marks = 80;
+         if (marks>= 90){
+            System.out.println("Grade: A");
+ }
+      else if(marks>= 70){
+       System.out.println("Grade: B");
+}
+     else if(marks>= 50){
+       System.out.println("Grade: C");
+ }
+     else if(marks>= 30){
+    System.out.println("Grade: D");
+ }
+        else{
+            System.out.println("Fail");
+ }
+}
+}
---
+++ Assignment no. 2/Variables.java
@@ -0,0 +1,20 @@
+class Variables{
+ public static void main(String[] args){
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 40;
+        float e = 50.5f;
+        double f = 60.5;
+        char g = 'A';
+        boolean h = true;
+    System.out.println("Byte: " + a);
+    System.out.println("Short: " + b);
+     System.out.println("Int: " + c);
+     System.out.println("Long: " + d);
+       System.out.println("Float: " + e);
+    System.out.println("Double: " + f);
+      System.out.println("Char: " + g);
+     System.out.println("Boolean: " + h);
+ }
+}
